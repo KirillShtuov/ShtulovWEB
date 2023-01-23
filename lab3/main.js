531833526e9bfe6d239e8b2196c41c5758c08253
KirillShtuov <123113011+KirillShtuov@users.noreply.github.com>
2023-01-23 22:33:59 +0300

    Add files via upload

---
+++ lab3/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn').addEventListener('click', function(){
+    for(let i = 0; i<document.querySelectorAll('.post').length; i++){
+        if(i % 2!= 0){
+            document.querySelectorAll('.post')[i].style.backgroundColor = 'green';
+        }
+    }    
+});
